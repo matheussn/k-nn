29c46522afca3587d990ba3f0b6075426042a937
Matheus <matheusneto23@gmail.com>
2019-09-24 19:31:05 -0300

    improvements

--- utils/knn.py
+++ utils/knn.py
@@ -4,15 +4,15 @@ def get_neighbors(arr: list, quantity: int):
     return arr[:quantity]
 
 
-def get_classification(neighbors: list, quantity: int):
+def get_classification(neighbors: list, quantity):
     nearest = get_neighbors(neighbors, quantity)
     nearest = [classification for i, classification in nearest]
     dict_classification = {key: nearest.count(key) for key in nearest}
-    value = max(dict_classification, key=dict_classification.get)
+    index = max(dict_classification, key=dict_classification.get)
 
     values = list(dict_classification.values())
 
-    if values.count(dict_classification[value]) > 1:
+    if values.count(dict_classification[index]) > 1:
         return False
 
-    return value
+    return index
