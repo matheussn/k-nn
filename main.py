273942a9d4d6c471ca3158b355d9c9041c13de95
Matheus <matheusneto23@gmail.com>
2019-09-24 21:31:02 -0300

    init main function

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import random
 import sys
 
+from utils.makeFolds import make_folds
 from utils.structure import structure_data
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 4:
         print("Você precisa informar o arquivo a ser lido!")
         exit(-1)
+
+    k_fold = int(sys.argv[2])
+    k_neighbors = int(sys.argv[3])
 
     data = structure_data(sys.argv[1])
     [quantity, dimension] = data[0]
     del data[0]
+    random.shuffle(data)
 
-    print(quantity)
-    print(dimension)
+    fold = make_folds(data, k_fold)
 
     print(data)
--- utils/makeFolds.py
+++ utils/makeFolds.py
@@ -1,5 +1,5 @@
 
-def make_Folds(sampling, k):
+def make_folds(sampling, k):
     
     aux = list(sampling[int(parte*len(sampling)/k):int((parte+1)*len(sampling)/k)] for parte in range(k))
     return dict(zip(list(range(k)),aux))
